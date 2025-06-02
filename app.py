3478a82bb22a42d1fb25b30bb725b4754cb42760
gnharris <133399925+gnharris@users.noreply.github.com>
2025-06-02 14:18:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ app.layout = create_layout()
 register_callbacks(app)
 
 if __name__ == "__main__":
-    app.run_server(debug=True, host='0.0.0.0', port=8050)
+    app.run(debug=True, host='0.0.0.0', port=8050)
